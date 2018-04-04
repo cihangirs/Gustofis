8f1cfdda2b3f95cbd4d85afbdc49d3edf3b29e01
Cihangir Sungur <cihangir.sungur@voden.com.tr>
2018-04-04 11:33:32 +0300

    remove crash line

--- GustOfis/AppDelegate.swift
+++ GustOfis/AppDelegate.swift
@@ -21,8 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let fourthViewController: FourthViewController = FourthViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        Fabric.with([Crashlytics.self])
 
         firstViewController.tabBarItem = UITabBarItem(title: "First", image: UIImage(named: "first"), tag: 0)
         secondViewController.tabBarItem = UITabBarItem(title: "Second", image: UIImage(named: "second"), tag: 0)
